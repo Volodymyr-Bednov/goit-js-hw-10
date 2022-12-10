7bac86098b97f23bc94e2fa917ec87a4d7b62e4f
Volodymyr-Bednov <109081369+Volodymyr-Bednov@users.noreply.github.com>
2022-12-10 14:57:38 +0000

    Deploying to gh-pages from @ Volodymyr-Bednov/goit-js-hw-10@a6cf1fb89f87fcffe3c45417c0f6a6b5a6e3b89c 🚀

--- index.3509273e.js
+++
@@ -1,2 +0,0 @@
-!function(){var n,o;document.querySelector("input#search-box");n="sw",o="https://restcountries.com/v3.1/name/",console.log(o+n),fetch("".concat(o).concat(n,"?fields=name,capital,population,flags,languages")).then((function(n){if(!n.ok)throw new Error(n.status);return n.json()})).then((function(n){console.log(n)}))}();
-//# sourceMappingURL=index.3509273e.js.map
--- index.9286883c.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("input#search-box");(e=>{const o="https://restcountries.com/v3.1/name/";console.log(o+e),fetch(`${o}${e}?fields=name,capital,population,flags,languages`).then((e=>{if(!e.ok)throw new Error(e.status);return e.json()})).then((e=>{console.log(e)}))})("sw");
-//# sourceMappingURL=index.9286883c.js.map
